81ced602eb4250666eaba8e0c245db54aa07e419
sagudev <samo.golez@outlook.com>
2020-11-27 08:41:02 +0100

    r

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+type F = fraction::Fraction;
+
 #[derive(Debug, Clone, Copy)]
 struct num {
     /// num
@@ -14,12 +16,6 @@ struct interval(num, num);
 impl interval {
     fn get_c(&self) -> f64 {
         (self.0.x + self.1.x) / 2.0
-    }
-    fn get_s(&self) -> f64 {
-        self.0.x
-    }
-    fn get_e(&self) -> f64 {
-        self.1.x
     }
     fn get_interval(self, n: num) -> Self {
         if self.0.sign.unwrap() != n.sign.unwrap() {
@@ -38,10 +34,11 @@ struct m {
 
 impl m {
     fn new(expr: String) -> Self {
+        meval::eval_str(expr.replace("x", &"(1)")).unwrap();
         Self { expr }
     }
     fn eval(&self, x: f64) -> f64 {
-        println!("{}", self.expr.replace("x", &(format!("({})", x))));
+        //println!("{}", self.expr.replace("x", &(format!("({})", x))));
         meval::eval_str(self.expr.replace("x", &(format!("({})", x)))).unwrap()
     }
     fn sign(&self, x: f64) -> Option<bool> {
@@ -70,6 +67,36 @@ impl m {
     fn c(&self, int: interval) -> num {
         self.get(int.get_c())
     }
+    fn mach(&self, int: &mut interval) {
+        for i in 0..10 {
+            let a = &int.0;
+            let b = &int.1;
+            println!("Interval iskanja ({}, {})", a.x, b.x);
+            if a.sign.is_none() {
+                println!("Dobili smo točno ničlo: x={}", a.x);
+            } else if b.sign.is_none() {
+                println!("Dobili smo točno ničlo: x={}", b.x);
+            } else {
+                if a.sign.unwrap() == b.sign.unwrap() {
+                    println!("Ne znam določiti");
+                } else {
+                    let c = self.c(*int);
+                    println!("c{} = {} = {}", i, F::from(c.x), c.x);
+                    if c.sign.is_none() {
+                        println!("c{} je ničla", i)
+                    } else {
+                        println!(
+                            "p(c{}) = {} kar je {}",
+                            i,
+                            c.p,
+                            display_bool(c.sign.unwrap())
+                        );
+                        *int = int.get_interval(c);
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn display_bool(b: bool) -> &'static str {
@@ -86,34 +113,20 @@ fn main() {
     //std::io::stdin().read_line(&mut line).unwrap();
     let m = m::new("x^3-3*x+1".to_string());
     //println!("Result: {}", m.eval(0.0));
+    // {:#.4}
     let mut int = m.interval(0.0, 1.0);
-    for i in 0..10 {
-        let a = &int.0;
-        let b = &int.1;
-        println!("Interval iskanja ({}, {})", a.x, b.x);
-        if a.sign.is_none() {
-            println!("Dobili smo točno ničlo: x={}", a.x);
-        } else if b.sign.is_none() {
-            println!("Dobili smo točno ničlo: x={}", b.x);
-        } else {
-            if a.sign.unwrap() == b.sign.unwrap() {
-                println!("Ne znam določiti");
-            } else {
-                println!("Določujem...");
-                let c = m.c(int);
-                println!("c{} = {}", i, c.x);
-                if c.sign.is_none() {
-                    println!("c{} je ničla", i)
-                } else {
-                    println!(
-                        "p(c{}) = {} kar je {}",
-                        i,
-                        c.p,
-                        display_bool(c.sign.unwrap())
-                    );
-                    int = int.get_interval(c);
-                }
-            }
-        }
-    }
+    m.mach(&mut int);
+    let a = &int.0;
+    let b = &int.1;
+    println!("Končni interval ({}, {})", a.x, b.x);
+}
+
+#[test]
+fn it_works() {
+    let m = m::new("x^3-3*x+1".to_string());
+    let mut int = m.interval(0.0, 1.0);
+    m.mach(&mut int);
+    let a = &int.0;
+    let b = &int.1;
+    println!("Končni interval ({}, {})", a.x, b.x);
 }
